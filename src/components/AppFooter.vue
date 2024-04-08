dda7201d17f796abf17bde92d6474677a7fedb41
Mihó Dániel <miho.daniel@mdy.hu>
2024-04-08 12:26:09 +0200

    reword footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -3,15 +3,16 @@
 <template>
   <footer class="text-xs">
     <p>
-      Made with 💙 by
-      <a href="https://github.com/dubbie" target="_blank" class="text-zinc-400">@dubbie</a>
+      <a href="https://github.com/dubbie" target="_blank" class="text-zinc-400">Source code</a>
     </p>
     <p>
       Thanks to
       <a href="https://github.com/ranelpadon/ngram-type" target="_blank" class="text-zinc-400"
         >ngram type</a
       >
-      for the base of the code
+      for the base of the code and
+      <a href="https://monkeytype.com/" target="_blank" class="text-zinc-400">Monkeytype</a> for the
+      inspiration
     </p>
   </footer>
 </template>
